54e40af07091a1c54fd202804c91c0ff11b2e673
Marek Čapistrák <marekcapistrak@MacBook-Pro-uzivatela-Marek.local>
2022-01-16 11:59:27 +0100

    add image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,31 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => (
   <Layout title="Homepage">
-    {
-      <p>
-        Anim dolore nostrud labore aute aliquip sint duis commodo do magna sunt
-        aliquip ipsum aliqua. Aliquip mollit commodo proident velit. Officia ex
-        sunt voluptate exercitation eiusmod commodo tempor ex et duis ad. Qui
-        aliqua aute cupidatat dolore elit irure labore cupidatat occaecat
-        cupidatat exercitation aute et. Voluptate nisi aute dolor eu. Velit
-        culpa veniam anim commodo dolore eiusmod ea eiusmod. Occaecat officia
-        consectetur in incididunt anim do duis.Dolor adipisicing voluptate
-        deserunt minim dolor ex nulla officia nulla eiusmod eiusmod officia.
-        Deserunt irure ea sint voluptate sit dolore esse. Dolor in est officia
-        officia labore. Nulla fugiat esse veniam irure laborum occaecat ea
-        consequat aliquip laboris ipsum non. Consequat in ut nulla ex enim
-        cupidatat culpa consequat elit quis consectetur ea ad. Est minim tempor
-        veniam do velit magna mollit quis aliquip.Reprehenderit exercitation
-        adipisicing excepteur nisi velit sint ex irure aliquip fugiat eu veniam
-        ea velit. Aliqua ad anim adipisicing amet aute reprehenderit. Quis in eu
-        cillum aute.
-      </p>
-    }
+    <p>
+      Anim dolore nostrud labore aute aliquip sint duis commodo do magna sunt
+      aliquip ipsum aliqua. Aliquip mollit commodo proident velit. Officia ex
+      sunt voluptate exercitation eiusmod commodo tempor ex et duis ad. Qui
+      aliqua aute cupidatat dolore elit irure labore cupidatat occaecat
+      cupidatat exercitation aute et. Voluptate nisi aute dolor eu. Velit culpa
+      veniam anim commodo dolore eiusmod ea eiusmod. Occaecat officia
+      consectetur in incididunt anim do duis.Dolor adipisicing voluptate
+      deserunt minim dolor ex nulla officia nulla eiusmod eiusmod officia.
+      Deserunt irure ea sint voluptate sit dolore esse. Dolor in est officia
+      officia labore. Nulla fugiat esse veniam irure laborum occaecat ea
+      consequat aliquip laboris ipsum non. Consequat in ut nulla ex enim
+      cupidatat culpa consequat elit quis consectetur ea ad. Est minim tempor
+      veniam do velit magna mollit quis aliquip.Reprehenderit exercitation
+      adipisicing excepteur nisi velit sint ex irure aliquip fugiat eu veniam ea
+      velit. Aliqua ad anim adipisicing amet aute reprehenderit. Quis in eu
+      cillum aute.
+    </p>
+    <StaticImage
+      alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+    />
   </Layout>
 )
 
